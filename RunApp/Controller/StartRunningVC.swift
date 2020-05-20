59e9186e92de02f561bd6d20938ffd94ff859b39
Catalin Vunvulea <catalin.vunvulea@gmail.com>
2020-05-20 09:15:34 +0100

    Initial Project setup and StartRunningVC

--- RunApp/Controller/StartRunningVC.swift
+++ RunApp/Controller/StartRunningVC.swift
@@ -1,5 +1,5 @@
 //
-//  FirstViewController.swift
+//  StartRunningVC.swift
 //  RunApp
 //
 //  Created by Dumitru Catalin Vunvulea on 20/05/2020.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class StartRunningVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
