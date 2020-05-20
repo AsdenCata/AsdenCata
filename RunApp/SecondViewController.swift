54a63bfa892fab9f2acc644af3338833ac413b20
Catalin Vunvulea <catalin.vunvulea@gmail.com>
2020-05-20 08:23:31 +0100

    Initial Commit

---
+++ RunApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  RunApp
+//
+//  Created by Dumitru Catalin Vunvulea on 20/05/2020.
+//  Copyright © 2020 Dumitru Catalin Vunvulea. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
