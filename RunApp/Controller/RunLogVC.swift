055560fac38048b838be9b5cfcd348dc8663fd92
Catalin Vunvulea <catalin.vunvulea@gmail.com>
2020-05-20 09:55:50 +0100

    set up the UI for the LogVC

--- RunApp/Controller/RunLogVC.swift
+++ RunApp/Controller/RunLogVC.swift
@@ -1,5 +1,5 @@
 //
-//  SecondViewController.swift
+//  RunLogVC.swift
 //  RunApp
 //
 //  Created by Dumitru Catalin Vunvulea on 20/05/2020.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class RunLogVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
