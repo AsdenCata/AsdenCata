1c90b24dc9436d57738ce03c51fe8b4a24185d69
Catalin Vunvulea <catalin.vunvulea@gmail.com>
2020-05-21 15:01:45 +0100

    Track Location
    func startRun, endRun, calculate distance, keep array with locations

--- RunApp/Controller/OnRunVC.swift
+++ RunApp/Controller/OnRunVC.swift
@@ -7,14 +7,24 @@
 //
 
 import UIKit
+import MapKit
 
 class OnRunVC: LocationVC {  //inherit everithing from LocationVC
     
     @IBOutlet weak var swipeBackgroundImg: UIImageView!
     @IBOutlet weak var sliderImg: UIImageView!
+    @IBOutlet weak var durationLbl: UILabel!
+    @IBOutlet weak var paceLbl: UILabel!
+    @IBOutlet weak var distanceLbl: UILabel!
+    @IBOutlet weak var pauseBtn: UIButton!
     
     
-
+    var startLocation: CLLocation!
+    var lastLocation: CLLocation!
+    
+    var runDistance = 0.0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let swipeGesture = UIPanGestureRecognizer(target: self, action: #selector(endRunSwiped(sender:)))
@@ -22,6 +32,27 @@ class OnRunVC: LocationVC {  //inherit everithing from LocationVC
         sliderImg.isUserInteractionEnabled = true
         swipeGesture.delegate = self as? UIGestureRecognizerDelegate
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        manager?.delegate = self
+        manager?.distanceFilter = 10     //distance will update each 10 meeters
+        onRun()
+    }
+    
+    @IBAction func pauseBtnPressed(_ sender: Any) {
+        
+    }
+    
+    
+    func onRun() {   //update location
+        manager?.startUpdatingLocation()
+    }
+    
+    func endRun() {
+        manager?.stopUpdatingLocation()
+    }
+    
     
     @objc func endRunSwiped(sender: UIPanGestureRecognizer) {
         let minAdjust: CGFloat = 80 //values are given based on how they look on the screen
@@ -33,7 +64,8 @@ class OnRunVC: LocationVC {  //inherit everithing from LocationVC
                     sliderView.center.x = sliderView.center.x + translation.x //seting the center of the slider to move according to the swipeGesture, between the min and max points(Adjust
                 } else if sliderView.center.x >= (swipeBackgroundImg.center.x + maxAdjust){
                     sliderView.center.x = swipeBackgroundImg.center.x + maxAdjust //adjust if slider got to far to right from view
-                     // TO DO: end running code goes here
+                    // TO DO: end running code goes here
+                    
                     dismiss(animated: true, completion: nil)
                 } else {
                     sliderView.center.x = swipeBackgroundImg.center.x - minAdjust    //adjust if slider go to far to left from view
@@ -50,5 +82,25 @@ class OnRunVC: LocationVC {  //inherit everithing from LocationVC
         
     }
     
+    
+}
+
+extension OnRunVC: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse || status == .authorizedAlways { //it is mandatory to check if user has approved to share location
+            checkLocationAuthStatus()
+        }
+    }
+    
+    // update the locations in an array; it will be called every time the location gets updated
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if startLocation == nil {
+            startLocation = locations.first     //we populate var startLocation with the first location
+        } else if let location = locations.last { // it means we have started to run, bun paused
+            runDistance = lastLocation.distance(from: location)  //setting the runDistance
+            distanceLbl.text = "\(runDistance)"
+        }
+        lastLocation = locations.last
+    }
     
 }
