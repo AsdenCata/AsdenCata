9511869eea546a0042ba33a3b450a7cb259e0ba0
Catalin Vunvulea <catalin.vunvulea@gmail.com>
2020-05-21 13:30:45 +0100

    slide button animated and set to return to initial value if not dragged to the end

--- RunApp/Controller/OnRunVC.swift
+++ RunApp/Controller/OnRunVC.swift
@@ -30,13 +30,20 @@ class OnRunVC: LocationVC {  //inherit everithing from LocationVC
             if sender.state == UIGestureRecognizer.State.began || sender.state == UIGestureRecognizer.State.changed {
                 let translation = sender.translation(in: self.view) // give us how many points we are adding or substracting from the current location
                 if sliderView.center.x >= (swipeBackgroundImg.center.x - minAdjust) && sliderView.center.x <= (swipeBackgroundImg.center.x + maxAdjust) {
-                    sliderView.center.x = sliderView.center.x + translation.x //seting the center of the slider to move according to the swipeGesture, between the min and max points(Adjust)
+                    sliderView.center.x = sliderView.center.x + translation.x //seting the center of the slider to move according to the swipeGesture, between the min and max points(Adjust
                 } else if sliderView.center.x >= (swipeBackgroundImg.center.x + maxAdjust){
-                    sliderView.center.x = swipeBackgroundImg.center.x + maxAdjust //adjust if slider go to far to right from view
+                    sliderView.center.x = swipeBackgroundImg.center.x + maxAdjust //adjust if slider got to far to right from view
+                     // TO DO: end running code goes here
+                    dismiss(animated: true, completion: nil)
                 } else {
-                    sliderView.center.x = swipeBackgroundImg.center.x - minAdjust//adjust if slider go to far to left from view
+                    sliderView.center.x = swipeBackgroundImg.center.x - minAdjust    //adjust if slider go to far to left from view
                 }
                 sender.setTranslation(CGPoint.zero, in: self.view) //set the translation value of the coordonate system in the specifil view
+            } else if sender.state == UIGestureRecognizer.State.ended {  //se the slider to return to initial value if not dragged to end
+                UIView.animate(withDuration: 0.2) {
+                    sliderView.center.x = self.swipeBackgroundImg.center.x - minAdjust
+                }
+                
             }
             
         }
